bcfab389752d7450ec1e4e273476c6044e55ebe0
Udaceue <rjemik3@gmail.com>
2022-07-30 22:08:19 +0200

    Task2 Beginner series#2 clock

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 console.log("Hi")
+// Clock shows h hours, m minutes and s seconds after midnight.
 
-//Complete the square sum function so that it squares each number passed into it and then sums 
-//the results together.
+// Your task is to write a function which returns the time since midnight in milliseconds.
 
-//For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+// Example:
+// h = 0
+// m = 1
+// s = 1
 
+// result = 61000
+// Input constraints:
 
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
 
-function squareSum(numbers){
-    let numlength = numbers.length;
-    let sum = 0;
-    for(i = 0; i<numlength; i++)
-    {
-     sum = sum + (Number(numbers[i])**2)
-    }
-    return sum
-}
-
-
-const inNumbers = [1,2,2]
-console.log(squareSum(inNumbers));
-
+function past(h, m, s){
+    //#Happy Coding! ^_^
+      const miliseconds = 1000;
+      const seconds = s * miliseconds; 
+      const minutes = (m * 1000)*60; 
+      const hours = (h* 1000)*3600;
+      const result = seconds + minutes + hours;
+    return result;
+  }
 
