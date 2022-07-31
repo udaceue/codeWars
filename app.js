a7ae096e7b38f31234e7fa8a85686d84c1b0dcb9
Udaceue <rjemik3@gmail.com>
2022-07-31 18:21:10 +0200

    Lost without a Map Task4

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
 console.log("Hi")
-// Given an array of integers.
 
-// Return an array, where the first element is the count of positives numbers and the 
-// second element is sum of negative numbers. 0 is neither positive nor negative.
+// Given an array of integers, return a new array with each value doubled.
 
-// If the input is an empty array or is null, return an empty array.
+// For example:
 
-// Example
-// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+// [1, 2, 3] --> [2, 4, 6]
 
-function countPositivesSumNegatives(input) {
-  
-    let resultArr = []
-    if(input === null || input.length===0){
-      return resultArr  
-    }
-  else{
-    const lengthArr = input.length;
-    let positives = 0
-    let negatives = 0 
-    for (i = 0; i<lengthArr; i++)
-    {if(input[i]>0)
-    {
-      positives = positives + 1
-    }
-     else if(input[i]<0)
-       {
-      negatives = negatives + input[i]
-       }
 
-     }
-  resultArr.push(positives, negatives)
-  console.log(resultArr)
-  return resultArr
-    }
+function maps(x){
+
+  let arr = []
+x.forEach(element=>{
+  arr.push(parseInt(element)*2)
+})
+return arr
 }
