f65c51b59a7f67e309417476e1da921bb5d9e429
Udaceue <rjemik3@gmail.com>
2022-08-01 09:03:43 +0200

    Task7 count by X

--- app.js
+++ app.js
@@ -1,43 +1,29 @@
 console.log("Hi");
 
-// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. 
-// If the array does not contain any numbers then you should return 0.
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
 
-// Examples
-// Input: [1, 5.2, 4, 0, -1]
-// Output: 9.2
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
 
-// Input: []
-// Output: 0
+// Return the results as an array (or list in Python, Haskell or Elixir).
 
-// Input: [-2.398]
-// Output: -2.398///
+// Examples:
 
-// Assumptions
-// You can assume that you are only given numbers.
-// You cannot assume the size of the array.
-// You can assume that you do get an array and if the array is empty, return 0.
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
 
 
-// // Sum Numbers
-// function sum (numbers) {
+function countBy(x, n) {
+  let z = [];
+  let multiples = 0
+  for (i = 0; i<n; i++)
+  {
+    multiples = multiples + x
+    z.push(multiples)
+  }
+    console.log(z)
   
-//   let result = 0 
-//       if (numbers === undefined){
-//         return 0 
-//       } else
-//         {
-//           numbers.map((el)=>{
-//             result = result + el
-//           })
-//           return result
-//         }
-// };
-
-const Numbers = [1,2,3,4]
-
-function sum(){
-        return Numbers.reduce((a,b) => a+b ,0)
+  return z;
 }
 
-console.log(sum(Numbers))
+console.log(countBy(2,5));
+
